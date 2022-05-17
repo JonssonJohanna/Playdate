a7238234cb9f8772912b5b28dd445333491b3b6e
JonssonJohannae <johanna_jonson@hotmail.com>
2022-05-17 14:20:20 +0200

    Added sound and created new file structure

---
+++ source/style.js
@@ -0,0 +1,14 @@
+import * as PIXI from 'pixi.js';
+// Text Object "Game over"
+export const gameOverStyle = new PIXI.TextStyle({
+  fontFamily: 'Montserrat',
+  fontSize: 48,
+  fill: 'red',
+  stroke: '#ffffff',
+  strokeThickness: 4,
+  dropShadow: true,
+  dropShadowDistance: 5,
+  dropShadowAngle: Math.PI / 2,
+  dropShadowBlur: 2,
+  dropShadowColor: '#000000',
+});
